e009c4ff359c4b5f480e6b6015837f89a576a0c6
wangneng <wangneng@xiaomi.com>
2025-07-29 10:43:03 +0800

    add multiply feature

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ def add(a, b):
 def minus(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
 
 if __name__ == "__main__":
     print(add(1,2))
     print(minus(10, 1))
+    print(multiply(2, 3))
