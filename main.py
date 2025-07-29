0adb97d92913578a01e013b7a1709f72bd3942ba
wangneng <wangneng@xiaomi.com>
2025-07-29 11:34:19 +0800

    feat: Add divide function with zero-division check

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ def minus(a, b):
 def multiply(a, b):
     return a * b
 
+def devide(a, b):
+    if b == 0:
+        print("Error: can not device by 0.")
+        return None
+    return a / b
+
 if __name__ == "__main__":
     print(add(1,2))
     print(minus(10, 1))
     print(multiply(2, 3))
+    print(devide(2, 1))
